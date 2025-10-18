f9e5c2a47a39577ed58f3baf24ed549c805f8c71
pkyriakou23 <panayiotiskyr2308@gmail.com>
2025-10-18 23:41:22 +0300

    add unit and integration testing

---
+++ RobotGameSimulation.Tests/RobotGameTests/Models/BoardTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using RobotGameSimulation.RobotGame.Enums;
+using RobotGameSimulation.RobotGame.Models;
+
+namespace RobotGameSimulation.Tests.RobotGameTests.Models
+{
+    public class BoardTests
+    {
+        [Fact]
+        public void PlaceRobot_ValidPosition_PlaceRobot()
+        {
+            var board = new Board();
+            var pos = new Position(2, 2);
+
+            board.PlaceRobot(pos, FacingDirection.EAST);
+
+            Assert.True(board.Robot!.IsPlaced);
+            Assert.Equal(pos, board.Robot.Position);
+            Assert.Equal(FacingDirection.EAST, board.Robot.FacingDirection);
+        }
+
+        [Fact]
+        public void PlaceRobot_InvalidPosition()
+        {
+            var board = new Board();
+            var pos = new Position(10, 2);
+
+            board.PlaceRobot(pos, FacingDirection.EAST);
+
+            Assert.Null(board.Robot);
+        }
+
+        [Fact]
+        public void PlaceRobot_ValidPositionWithWall_PlaceRobot()
+        {
+            var board = new Board();
+            var pos = new Position(2, 2);
+            var wallPosition = new Position(3, 3);
+
+            board.PlaceWall(wallPosition);
+            board.PlaceRobot(pos, FacingDirection.EAST);
+
+            Assert.True(board.Robot!.IsPlaced);
+            Assert.Equal(pos, board.Robot.Position);
+           Assert.NotEqual(wallPosition, board.Robot.Position);
+        }
+
+        /// <summary>
+        /// Tests that PlaceRobot ignores placement when position is occupied by a wall.
+        /// Also verifies that PlaceWall functionality works as expected.
+        /// </summary>
+        [Fact]
+        public void PlaceRobot_InvalidPositionBecauseOfWall()
+        {
+            var board = new Board();
+            var wallPosition = new Position(3, 3);
+
+            board.PlaceWall(wallPosition);
+            board.PlaceRobot(wallPosition, FacingDirection.EAST);
+
+            Assert.Null(board.Robot);
+        }
+
+        [Fact]
+        public void MoveRobot_UpdatePosition()
+        {
+            var board = new Board();
+            var pos = new Position(1, 2);
+            var newPos = new Position(1, 1);
+
+            board.PlaceRobot(pos, FacingDirection.WEST);
+            board.MoveRobot();
+
+            Assert.Equal(newPos, board.Robot!.Position);
+
+            newPos = new Position(1,5);
+            board.MoveRobot();
+            Assert.Equal(newPos, board.Robot!.Position);
+        }
+
+        [Fact]
+        public void MoveRobot_InvalidPositionBecauseOfWall()
+        {
+            var board = new Board();
+            var wallPosition = new Position(3, 3);
+            var pos = new Position(2, 3);
+
+            board.PlaceWall(wallPosition);
+            board.PlaceRobot(pos, FacingDirection.NORTH);
+            board.MoveRobot();
+
+            Assert.Equal(pos, board.Robot!.Position);
+        }
+
+        [Fact]
+        public void MoveRobot_InvalidRobotNotInPlace()
+        {
+            var board = new Board();
+            board.MoveRobot();
+
+            Assert.Null(board.Robot);
+        }
+
+    }
+}
---
+++ RobotGameSimulation.Tests/RobotGameTests/Models/PositionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using RobotGameSimulation.RobotGame.Models;
+using Xunit;
+
+namespace RobotGameSimulation.Tests.RobotGameTests.Models
+{
+    public class PositionTests
+    {
+        [Fact]
+        public void Positions_BeEqual()
+        {
+            var pos1 = new Position(2, 3);
+            var pos2 = new Position(2, 3);
+
+            Assert.Equal(pos1, pos2);
+            Assert.True(pos1.Equals(pos2));
+            Assert.Equal(pos1.GetHashCode(), pos2.GetHashCode());
+        }
+
+        [Fact]
+        public void Positions_NotBeEqual()
+        {
+            var pos1 = new Position(1, 2);
+            var pos2 = new Position(2, 1);
+
+            Assert.NotEqual(pos1, pos2);
+        }
+
+        [Fact]
+        public void ToString_CorrectFormat()
+        {
+            var pos = new Position(4, 5);
+            Assert.Equal("4,5", pos.ToString());
+        }
+    }
+}
